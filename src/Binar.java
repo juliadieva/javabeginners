19b1abdb044d757b472455052ede72bbcb67f2ae
juliadieva <julia.dieva@gmail.com>
2020-01-06 17:51:17 +0300

    task3

---
+++ src/Binar.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Binar {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число в бинарном формате: ");
+        byte num = in.nextByte(2);
+        int n= Integer.parseInt(String.valueOf(num),10);
+        System.out.print("Ваше число в десятичном формате: ");
+        System.out.print(n);
+        in.close();
+    }
+}
+
