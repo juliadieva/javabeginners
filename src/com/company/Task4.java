d73977e17fd60c4e194a6e61a842a57ca8facdd6
juliadieva <julia.dieva@gmail.com>
2020-01-10 00:16:34 +0300

    rename

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
-public class Binar {
+public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число в бинарном формате: ");
--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
-public class Tabl {
+public class Task5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
