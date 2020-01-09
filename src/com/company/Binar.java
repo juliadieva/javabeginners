2ae84dd97bbdef716da1a8ce7dbc66d1bf2f82c2
juliadieva <julia.dieva@gmail.com>
2020-01-09 23:16:16 +0300

    task5

--- src/com/company/Binar.java
+++ src/com/company/Binar.java
@@ -7,8 +7,7 @@ public class Binar {
         System.out.print("Введите число в бинарном формате: ");
         byte num = in.nextByte(2);
         int n= Integer.parseInt(String.valueOf(num),10);
-        System.out.print("Ваше число в десятичном формате: ");
-        System.out.print(n);
+        System.out.print("Ваше число в десятичном формате: " +n);
         in.close();
     }
 }
--- src/com/company/Tabl.java
+++ src/com/company/Tabl.java
@@ -6,26 +6,16 @@ public class Tabl {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
         int num = in.nextInt();
-        int z1=num*1;
-        int z2=num*2;
-        int z3=num*3;
-        int z4=num*4;
-        int z5=num*5;
-        int z6=num*6;
-        int z7=num*7;
-        int z8=num*8;
-        int z9=num*9;
-        int z10=num*10;
-        System.out.println("Таблица умножения: \n"+ "1*" + num+ "=" + z1);
-        System.out.println("2*" + num+ "=" + z2);
-        System.out.println("3*" + num+ "=" + z3);
-        System.out.println("4*" + num+ "=" + z4);
-        System.out.println("5*" + num+ "=" + z5);
-        System.out.println("6*" + num+ "=" + z6);
-        System.out.println("7*" + num+ "=" + z7);
-        System.out.println("8*" + num+ "=" + z8);
-        System.out.println("9*" + num+ "=" + z9);
-        System.out.println("10*" + num+ "=" + z10);
+        System.out.println("Таблица умножения: \n"+ "1*" + num+ "=" + num);
+        System.out.println("2*" + num+ "=" + num*2);
+        System.out.println("3*" + num+ "=" + num*3);
+        System.out.println("4*" + num+ "=" + num*4);
+        System.out.println("5*" + num+ "=" + num*5);
+        System.out.println("6*" + num+ "=" + num*6);
+        System.out.println("7*" + num+ "=" + num*7);
+        System.out.println("8*" + num+ "=" + num*8);
+        System.out.println("9*" + num+ "=" + num*9);
+        System.out.println("10*" + num+ "=" + num*10);
         in.close();
     }
 }
