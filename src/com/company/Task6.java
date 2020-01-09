39511baac3d170fd0a239c5a3977562833debd09
juliadieva <julia.dieva@gmail.com>
2020-01-10 00:03:58 +0300

    task6

---
+++ src/com/company/Task6.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task6
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число x");
+        int x = in.nextInt();
+        System.out.println("Введите число y");
+        int y = in.nextInt();
+        System.out.println("Введите число y");
+        int z = in.nextInt();
+        int a = (x+y+z)/3;
+        System.out.println("Среднее арифмитическое введенных чисел " + a);
+        int k=a/2;
+        if (k>3)
+        {
+            System.out.println("Программа выполнена корректно");
+        }
+    }
+}
