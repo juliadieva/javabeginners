7d02a08558cb025672b1826c27f184ce1b705437
juliadieva <julia.dieva@gmail.com>
2020-01-14 00:14:34 +0300

    task11

---
+++ src/Task11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String t = in.nextLine();
+        System.out.println(t);
+    }
+}
