5cf82b5c4f4eb0769349f3fba0b48e2adbe34155
juliadieva <julia.dieva@gmail.com>
2020-01-14 00:36:41 +0300

    task12

---
+++ src/Task12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String t = in.nextLine();
+        System.out.println(t.replaceAll(" ",
+                ""));
+    }
+}
